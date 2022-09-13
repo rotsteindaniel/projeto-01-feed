22264922aca32133476b2a0bbbe32dd8c9ec4b16
Daniel Rotstein Gonçalves <rotsteindaniel@gmail.com>
2022-09-13 12:24:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Daniel"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
+          />
+          <Post
+            author="Gabriel"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the"
+          />
+        </main>
+      </div>
+    </div>
+  );
+}
